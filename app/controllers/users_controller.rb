dd9c812e00f02881a46402c4409d0ee0abb97c16
karthickattabomman <karthickattabomman@gmail.com>
2021-06-24 15:01:10 +0530

    updated user controller and created destroy option for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   # GET /users
   def index
@@ -44,6 +46,12 @@ class UsersController < ApplicationController
 
 
   # DELETE /users/1
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
+  end
 
 
   private
@@ -55,5 +63,12 @@ class UsersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "You can only edit or delete your own account"
+        redirect_to @user
+      end
     end
 end
